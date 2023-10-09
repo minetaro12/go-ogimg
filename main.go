3439d970d2fe67835ca3853d5cbfcb5cbe24f187
minetaro12 <77948036+minetaro12@users.noreply.github.com>
2023-10-09 21:48:34 +0900

    migrate to Gin

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"go-ogimg/handlers"
 	"log"
-	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 var (
@@ -20,10 +21,15 @@ func init() {
 }
 
 func main() {
-	http.HandleFunc("/", handlers.Root)
+	gin.SetMode("release")
+	r := gin.Default()
+	r.GET("/", handlers.Root)
 
 	log.Println("Server Listening on", httpListen)
-	log.Fatal(http.ListenAndServe(httpListen, logRequest(http.DefaultServeMux)))
+	err := r.Run(httpListen)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func getEnv(key, fallback string) string {
@@ -36,11 +42,4 @@ func getEnv(key, fallback string) string {
 func fileExists(filename string) bool {
 	_, err := os.Stat(filename)
 	return err == nil
-}
-
-func logRequest(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-		handler.ServeHTTP(w, r)
-	})
 }
